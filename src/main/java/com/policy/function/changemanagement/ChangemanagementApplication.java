78a01a9a6ac6769f96ec04fa86a66fe13b586c84
Nirmith Anawaratne <nirmith.anawaratne@axiatadigitallabs.com>
2025-05-26 07:43:42 +0530

    Add Initial commit, with template files

---
+++ src/main/java/com/policy/function/changemanagement/ChangemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.policy.function.changemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/policy/function/changemanagement/ChangemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policy.function.changemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
